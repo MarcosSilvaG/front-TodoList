043189c58adfa0e00516e2161ac07c90dcef5252
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-06-02 21:42:58 -0500

    Creating the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
+import {Box,AppBar,Container,Toolbar,Typography,Button} from "@mui/material"
+import {Link, useNavigate} from "react-router-dom"
+
 export default function Navbar() {
+    
+    const navigate = useNavigate()
+    
     return (
-        <div>
-            Navbar
-        </div>
+        <Box sx= {{flexGrow: 1}}>
+            <AppBar position="static" color="transparent">
+                <Container>
+                    <Toolbar>
+                        <Typography sx={{flexGrow: 1}}>
+                            <Link to="/">PERN stack</Link>
+                        </Typography>
+                        <Button variant="contained" color="primary" onClick={() => navigate("task/new")}>
+                            New Task
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
     )
 }
