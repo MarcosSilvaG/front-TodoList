3cd5c97b92c8a0afaa9c2dad936ba77dd70ba2ec
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-06-03 16:03:34 -0500

    Adding the style for the taskform component

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,83 @@
+import {
+	Button,
+	Card,
+	CardContent,
+	Grid,
+	TextField,
+	Typography,
+} from "@mui/material";
+import {useState,useEffect} from "react"
+
 export default function TaskForm() {
+    const [task, setTask] = useState({
+        title:"",
+        description:"",
+    })
+    const handleSubmit =  (event) => {
+        event.preventDefault();
+            
+    }   
+    const handleChange = (event) => {
+        setTask({...task, [event.target.name]: event.target.value}); 
+    }
+
     return (
-        <div>
-            TaskForm
-        </div>
-    )
+		<Grid
+			container
+			direction="column"
+			alignItems="center"
+			justifyContent="center"
+		>
+			<Grid item xs={3}>
+				<Card
+					sx={{ mt: 7 }}
+					style={{
+						backgroundColor: "#1e272e",
+						padding: "1rem",
+					}}
+				>
+					<Typography variant="5" textAlign="center" color="white">
+						Create task
+					</Typography>
+					<CardContent>
+						<form onSubmit={handleSubmit}>
+							<TextField
+								variant="filled"
+								label="Write your title"
+								sx={{
+									display: "block",
+									margin: ".5rem 0",
+								}}
+                                name="title"
+                                onChange={handleChange}
+                                inputProps={{ style: { color: "white" } }}
+								InputLabelProps={{ style: { color: "white" } }}
+							/>
+							<TextField
+								variant="filled"
+								label="Write your description"
+								multiline
+								rows={4}
+								sx={{
+									display: "block",
+									margin: ".5rem 0",
+								}}
+                                name="description"
+                                onChange={handleChange}
+                                inputProps={{ style: { color: "white" } }}
+								InputLabelProps={{ style: { color: "white" } }}
+							/>
+							<Button
+								variant="contained"
+								color="primary"
+								type="submit"
+							>
+								Save
+							</Button>
+						</form>
+					</CardContent>
+				</Card>
+			</Grid>
+		</Grid>
+	);
 }
