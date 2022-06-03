171f935d0c2ee97285f15a77ad2cbbe2c1bffc96
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-06-03 16:05:16 -0500

    Formatting the app.js code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import TaskForm from "./components/TaskForm"
-import TaskList from "./components/TaskList"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList/>}/>
-        <Route path="/task/new" element={<TaskForm/>}/>
-        {/* the path to edit is missing  */}
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Navbar/>
+			<Container>
+				<Routes>
+					<Route path="/" element={<TaskList />} />
+					<Route path="/task/new" element={<TaskForm />} />
+					{/* the path to edit is missing  */}
+				</Routes>
+			</Container>
+		</BrowserRouter>
+	);
 }
