6c3eed3561c6da2aeb56b125a1fe4c3637a0f499
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-06-03 21:05:54 -0500

    adding styles and components to display the task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,58 @@
+import { Card, Typography, CardContent, Button } from "@mui/material";
+import { useEffect, useState } from "react";
+
 export default function TaskList() {
-    return (
-        <div>
-            TaskList
-        </div>
-    )
+	const [tasks, setTasks] = useState([]);
+
+	const loadTask = async () => {
+		const response = await fetch("http://localhost:4000/tasks");
+		const data = await response.json();
+		setTasks(data);
+	};
+	useEffect(() => {
+		loadTask();
+	}, []);
+	return (
+		<>
+			<h1>Task List</h1>
+			{tasks.map(task => (
+				<Card
+					style={{
+						marginBottom: ".7rem",
+						backgroundColor: "#1e272e",
+					}}
+				>
+					<CardContent
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+						}}
+					>
+						<div style={{color:"white"}}>
+							<Typography>{task.title}</Typography>
+							<Typography>{task.description}</Typography>
+						</div>
+
+						<div>
+							<Button
+								variant="contained"
+								color="inherit"
+								onClick={() => console.log("Editando")}
+							>
+								Edit
+							</Button>
+							<Button
+								variant="contained"
+								color="warning"
+								onClick={() => console.log("Eliminando")}
+								style={{ marginLeft: ".5rem" }}
+							>
+								Delete
+							</Button>
+						</div>
+					</CardContent>
+				</Card>
+			))}
+		</>
+	);
 }
